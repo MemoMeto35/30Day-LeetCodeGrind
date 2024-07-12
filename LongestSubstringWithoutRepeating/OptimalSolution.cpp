d02fc782505eac3c2ff7e5cc9c5eb1ad74eb0f0e
MemoMeto <t.a.metwally35@gmail.com>
2024-07-13 00:37:59 +0300

    Day 3

---
+++ LongestSubstringWithoutRepeating/OptimalSolution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.size()<=1){return s.size();}
+        int longest = 0; 
+        int left = 0;
+        unordered_map<char, int> hash; 
+        for(int right = 0; right < s.size(); right++){
+            char currentLetter = s[right];
+
+            if(hash.find(currentLetter) != hash.end() && hash[currentLetter]>=left){
+                left = hash[currentLetter]+1;
+            }
+            hash[currentLetter] = right; 
+            longest = max(longest, right - left +1);
+
+        }
+        return longest; 
+
+    }
+};
