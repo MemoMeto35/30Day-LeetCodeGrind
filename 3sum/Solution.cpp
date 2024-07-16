107972c31eb3eb113787bb9bf0e23fec88a51630
MemoMeto <t.a.metwally35@gmail.com>
2024-07-17 00:14:00 +0300

    Day 6

---
+++ 3sum/Solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> res;
+
+        if(nums.size() <=4 && nums[0]+nums[1]+nums[2]==0) return {{nums[0], nums[1], nums[2]}};
+
+        for(int i = 0; i<nums.size(); i++){
+            int j = i+1, k= nums.size()-1;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+            while(j<k){
+                if(nums[i]+nums[j]+nums[k]==0){
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1]) {j++;}
+                    
+                    while(j<k && nums[k]==nums[k+1]) {k--;}
+
+                }
+                else if(nums[i]+nums[j]+nums[k]<=0){
+                    j++;
+                    while(j<k && nums[j] == nums[j-1]) {j++;}
+                    
+                }
+                else{
+                    k--;
+                    while(j<k && nums[k]==nums[k+1]) {k--;}
+
+                }
+            }
+            
+        }
+        return res;
+        
+    }
+};
