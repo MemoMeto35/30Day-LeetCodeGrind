3a58bb806d04aa57668d4e264cd217f8e366d0b8
MemoMeto <t.a.metwally35@gmail.com>
2024-08-03 04:31:44 +0300

    Create factorialTrailingZeros.cpp

---
+++ __MathSolutions/factorialTrailingZeros.cpp
@@ -0,0 +1,14 @@
+// optimal solution O(log n)
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int res =0;
+        int powOf5 = 5;
+        while(n/powOf5){
+            
+            res += n/powOf5; 
+            powOf5 *= 5;
+        }
+        return res; 
+    }
+};
